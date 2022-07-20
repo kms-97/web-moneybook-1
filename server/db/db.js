4523a853fca331b9714a9389e10ddf05d38e768d
HyeonDoonge <migu554@naver.com>
2022-07-20 16:31:12 +0900

    chore: express 서버 환경 세팅

---
+++ server/db/db.js
@@ -0,0 +1,15 @@
+const mysql = require('mysql2/promise');
+
+const options = {
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+};
+
+const connectionPool = mysql.createPool(options);
+function getConnection() {
+  return connectionPool.getConnection();
+}
+
+module.exports = getConnection;
