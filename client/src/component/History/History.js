3928f89e8f92453da7ec65eb9b01f405ed96371f
kms-97 <msg016@naver.com>
2022-07-21 13:47:12 +0900

    style: 일 단위 내역 UI 추가

--- client/src/component/History/History.js
+++ client/src/component/History/History.js
@@ -28,7 +28,48 @@ export class History {
           </li>
         </ul>
       </div>
-      <ul class="list"></ul>
+      <ul class="list">
+        <li>
+          <div class='subtitle'>
+            <div class='date'>7월 15일 <span class='week'>목</span></div>
+            <div class='sum'>
+                <div class='income'>수입 12,324</div>
+                <div class='cost'>지출 583,495</div>
+            </div>
+          </div>
+          <table class='item'>
+            <tr class='active'>
+                <td class='category'><span class='culture'>문화/여가</span></td>
+                <td class='content'>세미나 신청</td>
+                <td class='payment'>현대카드</td>
+                <td class='amount'>50,000</td>
+            </tr>
+            <tr>
+                <td class='category'><span class='traffic'>교통</span></td>
+                <td class='content'>세미나 신청</td>
+                <td class='payment'>현대카드</td>
+                <td class='amount income'>1,250,000</td>
+            </tr>
+          </table>
+        </li>
+        <li>
+            <div class='subtitle'>
+            <div class='date'>7월 14일 <span class='week'>수</span></div>
+            <div class='sum'>
+                <div class='income'>수입 12,324</div>
+                <div class='cost'>지출 583,495</div>
+            </div>
+            </div>
+            <table class='item'>
+            <tr>
+                <td class='category'><span class='food'>식비</span></td>
+                <td class='content'>세미나 신청</td>
+                <td class='payment'>현대카드</td>
+                <td class='amount cost'>50,000</td>
+            </tr>
+            </table>
+        </li>
+      </ul>
     `;
   }
 }
