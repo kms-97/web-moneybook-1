a883dae68123d09ae21755573d761b0984f042bf
kms-97 <msg016@naver.com>
2022-07-22 13:36:32 +0900

    feat: Obsrevable 구조 변경

--- client/src/store/store.js
+++ client/src/store/store.js
@@ -1,52 +1,50 @@
 import useState from '../utils/Observable';
 
-export default {
-  historyInfo: useState({
-    month: '',
-    year: '',
-    history: [],
-  }),
-  category: useState([
-    {
-      id: 1,
-      content: '월급',
-      isIncome: true,
-    },
-    {
-      id: 2,
-      content: '용돈',
-      isIncome: true,
-    },
-    {
-      id: 3,
-      content: '식비',
-      isIncome: false,
-    },
-    {
-      id: 4,
-      content: '생활',
-      isIncome: false,
-    },
-    {
-      id: 5,
-      content: '교통',
-      isIncome: false,
-    },
-    {
-      id: 6,
-      content: '여가',
-      isIncome: false,
-    },
-  ]),
-  payment: useState([
-    {
-      content: '현대카드',
-      id: 1,
-    },
-    {
-      content: '국민카드',
-      id: 2,
-    },
-  ]),
-  selectedId: useState(null),
-};
+export const dateStore = useState({
+  month: 7,
+  year: 2022,
+});
+export const historyStore = useState([]);
+export const categoryStore = useState([
+  {
+    id: 1,
+    content: '월급',
+    isIncome: true,
+  },
+  {
+    id: 2,
+    content: '용돈',
+    isIncome: true,
+  },
+  {
+    id: 3,
+    content: '식비',
+    isIncome: false,
+  },
+  {
+    id: 4,
+    content: '생활',
+    isIncome: false,
+  },
+  {
+    id: 5,
+    content: '교통',
+    isIncome: false,
+  },
+  {
+    id: 6,
+    content: '여가',
+    isIncome: false,
+  },
+]);
+export const paymentStore = useState([
+  {
+    content: '현대카드',
+    id: 1,
+  },
+  {
+    content: '국민카드',
+    id: 2,
+  },
+]);
+export const selectedHistoryStore = useState({});
