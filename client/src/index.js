33ed6fb26375fb5b8a851e224375680e606f285d
kms-97 <msg016@naver.com>
2022-07-22 13:40:25 +0900

    feat: store 구조 변경에 따른 초기 history 데이터 설정 코드 개선

--- client/src/index.js
+++ client/src/index.js
@@ -3,15 +3,9 @@ import './index.scss';
 
 import { getMockHistory } from './utils/generateMockData';
 import { HistoryView } from './view/HistoryView';
-import store from './store/store.js';
+import { historyStore } from './store/store.js';
 
 new HistoryView(document.getElementById('root'));
 
 // init
-getMockHistory(7).then((result) =>
-  store.historyInfo.subscribe()[1]({
-    year: 2022,
-    month: 7,
-    history: [...result],
-  }),
-);
+getMockHistory(7).then((result) => historyStore.set(result));
