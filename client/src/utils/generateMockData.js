ce951a1042d1880a539f1e76f012947b654cd6f9
unknown <msg016@naver.com>
2022-07-24 23:38:53 +0900

    feat: 카테고리, 결재수단 예시 데이터 추가

--- client/src/utils/generateMockData.js
+++ client/src/utils/generateMockData.js
@@ -1,4 +1,4 @@
-export function getMockHistory(month) {
+export function getMockHistory() {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
       const data = [];
@@ -7,11 +7,7 @@ export function getMockHistory(month) {
         data.push({
           id: i + 1,
           content: '목업 데이터',
-          date: `2022-${(month + '').padStart(2, 0)}-${(
-            Math.floor(Math.random() * 30) +
-            1 +
-            ''
-          ).padStart(2, 0)}`,
+          date: Math.floor(Math.random() * 30) + 1,
           categoryId: Math.floor(Math.random() * 5) + 1,
           paymentId: Math.floor(Math.random() + 1.5),
           amount: 30000,
@@ -36,6 +32,52 @@ export function getMockHistory(month) {
           return { date: v[0], datas: v[1] };
         }),
       );
-    }, 0);
+    }, 1000);
   });
+}
+
+export function getMockPayment() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => resolve([
+      {id: 1, content:'국민카드'},
+      {id: 2, content:'신한카드'}
+    ]), 1000);
+  })
+}
+
+export function getMockCategory() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => resolve([
+      {
+        id: 1,
+        content: '월급',
+        isIncome: true,
+      },
+      {
+        id: 2,
+        content: '용돈',
+        isIncome: true,
+      },
+      {
+        id: 3,
+        content: '식비',
+        isIncome: false,
+      },
+      {
+        id: 4,
+        content: '생활',
+        isIncome: false,
+      },
+      {
+        id: 5,
+        content: '교통',
+        isIncome: false,
+      },
+      {
+        id: 6,
+        content: '여가',
+        isIncome: false,
+      },
+    ]), 1000);
+  })
 }
