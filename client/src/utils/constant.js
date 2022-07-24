d037a4b9c4cbb03d4fe8d57b816f75ed208ae4c0
unknown <msg016@naver.com>
2022-07-24 21:09:48 +0900

    feat: 각 state의 key를 상수로 선언

---
+++ client/src/utils/constant.js
@@ -0,0 +1,10 @@
+export const storeKeys = {
+    CURRENT_DATE: 'date',
+    CURRENT_MONTH: 'month',
+    CURRENT_YEAR: 'year',
+    CURRENT_HISTORY: 'history',
+    LAST_SIX_HISTORY: 'lastSixMonthHistory',
+    CATEGORY: 'category',
+    PAYMENT: 'payment',
+    SELECTED_HISTORY: 'selectedHistory',
+}
