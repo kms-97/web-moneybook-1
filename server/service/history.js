02245f714cb00e26e693eb4db704e35d71fe5339
unknown <msg016@naver.com>
2022-07-26 00:09:20 +0900

    feat: post /history service 구현

--- server/service/history.js
+++ server/service/history.js
@@ -1,5 +1,5 @@
 const { getConnection } = require('../db/db');
-const { getStartAndEndDateString } = require('../util/date');
+const { getStartAndEndDateString, makeDateString } = require('../util/date');
 
 async function getAllHistoryOfMonth({ year, month }) {
   let connection;
@@ -15,12 +15,65 @@ async function getAllHistoryOfMonth({ year, month }) {
   }
 }
 
+async function postHistory({
+  year,
+  month,
+  date,
+  categoryId,
+  content,
+  paymentId,
+  amount,
+  isIncome,
+}) {
+  const createdDate = makeDateString({ year, month, date });
+  let connection;
+
+  try {
+    connection = await getConnection();
+    await connection.beginTransaction();
+
+    const [{ affectedRows }] = await insert(connection, {
+      createdDate,
+      categoryId,
+      content,
+      paymentId,
+      amount,
+      isIncome,
+    });
+    const [rows] = await findAllOfMonth(connection, { year, month });
+
+    await connection.commit();
+    return rows;
+  } catch (e) {
+    await connection.rollback();
+    throw e;
+  } finally {
+    connection?.release();
+  }
+}
+
 function findAllOfMonth(connection, { year, month }) {
   const { startDate, endDate } = getStartAndEndDateString({ year, month });
   const query = `select * from hist where create_date between ? and ?`;
   return connection.execute(query, [startDate, endDate]);
 }
 
+function insert(
+  connection,
+  { createdDate, categoryId, content, paymentId, amount, isIncome },
+) {
+  const query = `insert into hist (create_date, category_id, content, payment_id, amount, is_income) values (?, ?, ?, ?, ?, ?)`;
+  return connection.execute(query, [
+    createdDate,
+    categoryId,
+    content,
+    paymentId,
+    amount,
+    isIncome,
+  ]);
+}
+
 module.exports = {
   getAllHistoryOfMonth,
+  postHistory,
 };
