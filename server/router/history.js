f502889ffbe653da6832f7f3319a7fdbdc78e46e
kms-97 <msg016@naver.com>
2022-07-25 17:20:42 +0900

    fix: 응답 메소드 json으로 수정

--- server/router/history.js
+++ server/router/history.js
@@ -6,13 +6,13 @@ historyRouter.get('/', (req, res) => {
   res.json('get /history');
 });
 historyRouter.get('/SixMonthPerCategory', (req, res) => {
-  res.send('get /history/SixMonth');
+  res.json('get /history/SixMonth');
 });
 historyRouter.post('/', (req, res) => {
-  res.send('post /history');
+  res.json('post /history');
 });
 historyRouter.put('/', (req, res) => {
-  res.send('put /history');
+  res.json('put /history');
 });
 
 module.exports = { historyRouter };
