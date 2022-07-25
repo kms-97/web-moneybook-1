6efb88d74d558006902ba35148e15e77f249fc2f
kms-97 <msg016@naver.com>
2022-07-25 16:37:07 +0900

    feat: 모든 경로에 대해 index페이지를 반환

--- server/app.js
+++ server/app.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const logger = require('morgan');
+const path = require('path');
 const app = express();
 const port = 3000;
 require('dotenv').config();
 
 app.use(logger('dev'));
+app.use(express.static(path.join(__dirname, 'dist')));
+
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.send('index.html');
 });
 
 app.listen(port, () => {
