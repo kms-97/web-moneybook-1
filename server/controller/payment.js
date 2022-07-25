c57727705200c24e4d4f4a6b911744c501c2f641
kms-97 <msg016@naver.com>
2022-07-25 20:58:14 +0900

    feat: delete /payment controller 구현

--- server/controller/payment.js
+++ server/controller/payment.js
@@ -19,7 +19,18 @@ async function postPayment(req, res) {
   }
 }
 
+async function deletePayment(req, res) {
+  const { id } = req.body;
+  try {
+    const data = await service.deletePayment({ id });
+    res.status(200).json(data);
+  } catch (e) {
+    res.status(500).json(e.message);
+  }
+}
+
 module.exports = {
   getAllPayment,
   postPayment,
+  deletePayment,
 };
