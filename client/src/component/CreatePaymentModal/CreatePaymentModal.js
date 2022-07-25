021b567e21a76fa672cd401a3bd1c88c31b58322
Hyeondoonge <migu554@naver.com>
2022-07-26 01:11:39 +0900

    feat: 결제 수단 추가 모달 생성

---
+++ client/src/component/CreatePaymentModal/CreatePaymentModal.js
@@ -0,0 +1,61 @@
+import { createPayment } from '../../controller';
+import './CreatePaymentModal.scss';
+
+export class CreatePaymentModal {
+  constructor($target) {
+    this.$target = $target;
+    this.$paymentModal = document.createElement('div');
+    this.$paymentModal.className = 'payment-modal';
+
+    this.$target.appendChild(this.$paymentModal);
+    this.init();
+    this.render();
+  }
+
+  init() {
+    this.$paymentModal.addEventListener('click', (event) =>
+      this.onClickCreateButton(event),
+    );
+    this.$paymentModal.addEventListener('input', (event) => {
+      const $payment = this.$paymentModal.querySelector('input[name=payment]');
+      $payment.value = event.target.value;
+    });
+    this.$paymentModal.addEventListener('click', (event) =>
+      this.onClickCancelButton(event),
+    );
+  }
+
+  onClickCreateButton(event) {
+    const $createButton = event.target.closest('.create-button');
+    if (!$createButton) return;
+    const payment = this.$paymentModal.querySelector(
+      'input[name=payment]',
+    ).value;
+
+    createPayment(payment, () => {
+      this.$paymentModal.style.display = 'none';
+      const $payment = this.$paymentModal.querySelector('input[name=payment]');
+      $payment.value = '';
+    });
+  }
+
+  onClickCancelButton(event) {
+    const $cancelButton = event.target.closest('.cancel-button');
+    if (!$cancelButton) return;
+    const $payment = this.$paymentModal.querySelector('input[name=payment]');
+    $payment.value = '';
+    this.$paymentModal.style.display = 'none';
+  }
+
+  render() {
+    this.$paymentModal.innerHTML = `
+    <div class="payment-input-wrapper">
+      <span class="description">추가하실 결제수단을 적어주세요.</span>
+      <input type="text" name="payment" placeholder="입력하세요." autocomplete="off">
+      <div class="button-wrapper">
+        <button class="cancel-button">취소</button>
+        <button class="create-button">등록</button>
+      </div>
+    </div>`;
+  }
+}
