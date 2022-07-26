a07560765a1959c01fce4ca90e71f9352d172ec6
kms-97 <msg016@naver.com>
2022-07-26 20:01:52 +0900

    feat: 404, 500 에러 처리 로직 추가

--- server/controller/payment.js
+++ server/controller/payment.js
@@ -1,31 +1,31 @@
 const service = require('../service/payment');
 
-async function getAllPayment(req, res) {
+async function getAllPayment(req, res, next) {
   try {
     const data = await service.getAllPayment();
     res.status(200).json(data);
   } catch (e) {
-    res.status(500).json(e.message);
+    next(e);
   }
 }
 
-async function postPayment(req, res) {
+async function postPayment(req, res, next) {
   const { content } = req.body;
   try {
     const data = await service.postPayment({ content });
     res.status(200).json(data);
   } catch (e) {
-    res.status(500).json(e.message);
+    next(e);
   }
 }
 
-async function deletePayment(req, res) {
+async function deletePayment(req, res, next) {
   const { id } = req.body;
   try {
     const data = await service.deletePayment({ id });
     res.status(200).json(data);
   } catch (e) {
-    res.status(500).json(e.message);
+    next(e);
   }
 }
 
