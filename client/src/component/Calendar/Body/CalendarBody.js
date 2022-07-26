292f65be4c78ca3f83e665c3985de02d38d3d0ad
kms-97 <msg016@naver.com>
2022-07-27 03:36:06 +0900

    feat: header, body로 컴포넌트 분리

---
+++ client/src/component/Calendar/Body/CalendarBody.js
@@ -0,0 +1,60 @@
+import { subscribeState } from '../../../controller';
+import { storeKeys } from '../../../utils/constant';
+
+export class CalendarBody {
+  constructor($target) {
+    this.$target = $target;
+    this.$body = document.createElement('table');
+    this.$body.className = 'calendar-body';
+
+    this.unsubscribeDate = subscribeState({
+      key: storeKeys.CURRENT_DATE,
+      callback: () => this.render(),
+    });
+
+    this.$target.appendChild(this.$body);
+    this.init();
+    this.render();
+  }
+
+  init() {}
+
+  render() {
+    this.$body.innerHTML = `
+        <tr>
+            <td class='today'>
+            <div class='calendar-amount'>
+                <div class='calendar-amount-income'>130,000</div>
+                <div class='calendar-amount-cost'>20,000</div>
+                <div class='calendar-amount-total'>110,000</div>
+            </div>
+            <div class='calendar-date'>1</div>
+            </td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+        </tr>
+        <tr>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+        </tr>
+        <tr>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+        </tr>
+      `;
+  }
+}
--- client/src/component/Calendar/Calendar.js
+++ client/src/component/Calendar/Calendar.js
@@ -1,4 +1,6 @@
 import './Calendar.scss';
+import { CalendarBody } from './Body/CalendarBody';
+import { CalendarHeader } from './Header/CalenderHeader';
 
 export class Calendar {
   constructor($target) {
@@ -14,52 +16,7 @@ export class Calendar {
   init() {}
 
   render() {
-    this.$calendar.innerHTML = `
-        <div class='calendar-header'>
-            <div class='calendar-header-day sunday'>일</div>
-            <div class='calendar-header-day'>월</div>
-            <div class='calendar-header-day'>화</div>
-            <div class='calendar-header-day'>수</div>
-            <div class='calendar-header-day'>목</div>
-            <div class='calendar-header-day'>금</div>
-            <div class='calendar-header-day saturday'>토</div>
-        </div>
-        <table class='calendar-body'>
-            <tr>
-                <td class='today'>
-                  <div class='calendar-amount'>
-                    <div class='calendar-amount-income'>130,000</div>
-                    <div class='calendar-amount-cost'>20,000</div>
-                    <div class='calendar-amount-total'>110,000</div>
-                  </div>
-                  <div class='calendar-date'>1</div>
-                </td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-            </tr>
-            <tr>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-            </tr>
-            <tr>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-            </tr>
-        </table>
-    `;
+    new CalendarHeader(this.$calendar);
+    new CalendarBody(this.$calendar);
   }
 }
