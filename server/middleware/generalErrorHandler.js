2285c80afb222c868784ca9030c4c10812b9f20f
kms-97 <msg016@naver.com>
2022-07-26 20:00:42 +0900

    fix: 전역 에러처리 미들웨어의 반환 코드 수정

--- server/middleware/generalErrorHandler.js
+++ server/middleware/generalErrorHandler.js
@@ -1,5 +1,7 @@
 function generalErrorHandler(err, req, res, next) {
-  res.status(err.statusCode).json({ type: err.name, message: err.message });
+  res
+    .status(err.statusCode)
+    .json({ errorCode: 'E0001', type: err.name, message: err.message });
 }
 
 module.exports = {
