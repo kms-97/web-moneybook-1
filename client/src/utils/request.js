1b330de2c5a89d19cb4f3030f758e5115d273d60
HyeonDoonge <migu554@naver.com>
2022-07-26 18:08:50 +0900

    feat: request 템플릿 추가

---
+++ client/src/utils/request.js
@@ -0,0 +1,26 @@
+const BASE_URL = 'http://localhost:3000';
+
+export async function request({ method, requestURL, body }) {
+  const URL = `${BASE_URL}${requestURL}`;
+  const options = {
+    method,
+    headers: {
+      'content-type': 'application/json',
+    },
+    body: body ? JSON.stringify(body) : undefined,
+  };
+
+  try {
+    const response = await fetch(URL, options);
+    if (!response.ok) {
+      const { name, message } = await response.json();
+      const customError = new Error(message);
+      customError.name = name;
+      throw customError;
+    }
+    return response.json();
+  } catch (error) {
+    console.log(error);
+    return error;
+  }
+}
