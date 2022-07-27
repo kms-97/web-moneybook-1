e64c13a4ea82f2a8d6d38929b9ee18b71fbda535
kms-97 <msg016@naver.com>
2022-07-27 17:26:51 +0900

    feat: 카테고리 색상 상수처리

--- client/src/utils/constant.js
+++ client/src/utils/constant.js
@@ -9,4 +9,17 @@ export const storeKeys = {
   SELECTED_HISTORY: 'selectedHistory',
 };
 
+export const CategoryColor = {
+  1: '#B9D58C',
+  2: '#E6D267',
+  3: '#E2B765',
+  4: '#4A6CC3',
+  5: '#4CA1DE',
+  6: '#94D3CC',
+  7: '#4CB8B8',
+  8: '#6ED5EB',
+  9: '#D092E2',
+  10: '#817DCE',
+};
+
 export const WEEK_LENGTH = 7;
