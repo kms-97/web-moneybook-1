7711abc88be996f85c6e8858a164d0094840667e
kms-97 <msg016@naver.com>
2022-07-29 04:14:58 +0900

    feat: Component 클래스 정의

---
+++ client/src/core/Component.js
@@ -0,0 +1,42 @@
+import { subscribeState } from '../controller';
+
+export default class Component {
+  constructor($parent, elementType, attributes = {}, props = {}) {
+    this.$parent = $parent;
+    this.$self = document.createElement(elementType);
+    this.attributes = attributes;
+    this.unsubscribers = [];
+    this.props = props;
+
+    this.$parent.appendChild(this.$self);
+    this.init();
+  }
+
+  init() {
+    this.setAttribute();
+    this.attachEvents();
+    this.render(this.props);
+  }
+
+  setAttribute() {
+    Object.entries(this.attributes).forEach(([name, value]) =>
+      this.$self.setAttribute(name, value),
+    );
+  }
+
+  subscribeState(keys) {
+    keys.forEach((key) => {
+      this.unsubscribers.push(
+        subscribeState({ key, callback: () => this.render() }),
+      );
+    });
+  }
+
+  attachEvents() {}
+
+  cleanUp() {
+    this.unsubscribers.forEach((fn) => fn());
+  }
+
+  render() {}
+}
