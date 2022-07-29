d9527e56ddadf488bb53e1e85a4eee9db16baf7a
kms-97 <msg016@naver.com>
2022-07-29 10:12:26 +0900

    feat: 달력 푸터 추가

--- client/src/component/Calendar/Calendar.js
+++ client/src/component/Calendar/Calendar.js
@@ -5,12 +5,13 @@ import { getState } from '../../controller';
 import { storeKeys } from '../../utils/constant';
 import { LoadingIndicator } from '../LoadingIndicator/LoadingIndicator';
 import Component from '../../core/Component';
+import CalendarFooter from './Footer/CalendarFooter';
 
 export class Calendar extends Component {
   constructor($parent) {
     super($parent, 'main', { class: 'calendar' });
 
-    this.subscribeState([storeKeys.ISLOADING]);
+    this.subscribeState([storeKeys.ISLOADING, storeKeys.CURRENT_HISTORY]);
   }
 
   render() {
@@ -24,5 +25,6 @@ export class Calendar extends Component {
 
     new CalendarHeader(this.$self);
     new CalendarBody(this.$self);
+    new CalendarFooter(this.$self);
   }
 }
